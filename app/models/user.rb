37d2f5ab0728487f43a30bab590441f358f40984
Stephanie Kamen <st.kamen@gmx.de>
2021-08-14 16:17:31 +0200

    deleted valitadion styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, presence: true
   validates :email, presence: true
-  # validates :style, inclusion: { in: %w(ashtanga hatha vinyasa yin) }
   # has_many :bookings, dependent: :destroy
   # has_many :courses, dependent: :destroy
 end
