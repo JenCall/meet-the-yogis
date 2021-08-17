271015a13c81a0db2431e79c21972cee9dc33b25
Meriem Manouchi <Meriem.manouchi@gmail.com>
2021-08-17 22:39:33 +0200

    Added PG search, updated pages controller and view, and user model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+  #  @teachers = User.addressbased_teacher_search(params[:query])
   end
+
+
 end
