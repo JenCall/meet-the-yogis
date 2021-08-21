1981ea539fa1117a7003b6db68256a4358a6c7e0
Meriem Manouchi <Meriem.manouchi@gmail.com>
2021-08-21 11:15:04 +0200

    Booking and course seed done

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,9 @@
 class Course < ApplicationRecord
-  belongs_to :User.teachers
+  belongs_to :user
   has_many :bookings, dependent: :destroy
 
   # validates :styles, :title, :address, :description, :price, :language, :availability, :date, :start_time, :end_time, presence: true
-  validates :title, length: { minimum: 3 }
-  validates :description, length: { minimum: 15 }
+  # validates :title, length: { minimum: 3 }
+  # validates :description, length: { minimum: 15 }
   has_one_attached :photo
 end
