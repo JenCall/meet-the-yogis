0da1e96be89fc4be4b559f99d84abebfde603e9d
Stephanie Kamen <st.kamen@gmx.de>
2021-08-29 09:49:45 +0200

    fixed duplicate code

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -43,11 +43,11 @@ class TeachersController < ApplicationController
     # @booking = Booking.new
     authorize @teacher
 
-    @teacher.courses.each do |course|
-      location = Geocoder.search(course.address)
-      course.latitude = location.first.coordinates[0]
-      course.longitude = location.first.coordinates[1]
-    end
+    # @teacher.courses.each do |course|
+    #   location = Geocoder.search(course.address)
+    #   course.latitude = location.first.coordinates[0]
+    #   course.longitude = location.first.coordinates[1]
+    # end
     # @markers={lat:location.first.coordinates[0], lng:location.first.coordinates[1] }
 
     @markers = @teacher.courses.geocoded.map do |course|
