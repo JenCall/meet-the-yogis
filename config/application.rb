9a6714d46529cc767811b96e56f70250b7bb8237
Jeniffer Callou <jenifferprc@gmail.com>
2021-09-04 10:44:10 +0200

    added line

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,7 @@ module MeetTheYogis
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+  config.assets.initialize_on_precompile = false
   end
-
-config.assets.initialize_on_precompile = false
-
 end
